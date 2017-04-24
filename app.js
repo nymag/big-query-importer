7e249a110116c6ed3aeb42d27d2e519a39ef590f
dmabuada <abuadas.dalia@gmail.com>
2017-04-24 12:08:33 -0400

    Make importer executable

--- app.js
+++ app.js
@@ -7,33 +7,37 @@ const pages = require('./lib/pages.js'),
   schemaFile = path.join(__dirname, './data/schema.json'),
   schema = fs.readFileSync(schemaFile, 'utf8'),
   options = JSON.parse(schema),
-  yargs = require('yargs')
-  .options({
-    url: {
-      alias: 'u',
-      demandOption: true,
-      describe: 'Provide a Clay page url',
-      string: true
-    },
-    dataset: {
-      alias: 'd',
-      demandOption: true,
-      describe: 'Provide the name of a BigQuery dataset',
-      string: true
-    },
-    table: {
-      alias: 't',
-      demandOption: true,
-      describe: 'Provide the name of a BigQuery table',
-      string: true
-    }
-  })
-  .help('h')
-  .alias('h', 'help')
-  .argv;
+  cli = function() {
+    yargs = require('yargs')
+    .options({
+      url: {
+        alias: 'u',
+        demandOption: true,
+        describe: 'Provide a Clay page url',
+        string: true
+      },
+      dataset: {
+        alias: 'd',
+        demandOption: true,
+        describe: 'Provide the name of a BigQuery dataset',
+        string: true
+      },
+      table: {
+        alias: 't',
+        demandOption: true,
+        describe: 'Provide the name of a BigQuery table',
+        string: true
+      }
+    })
+    .help('h')
+    .alias('h', 'help')
+    .argv;
 
-pages.page(yargs.url)
-.then((data) => {
-  return bq.insertDataAsStream(yargs.dataset, yargs.table, options, data);
-})
+  pages.page(yargs.url)
+  .then((data) => {
+    return bq.insertDataAsStream(yargs.dataset, yargs.table, options, data);
+  })
+};
+
+module.exports.cli = cli;
 
