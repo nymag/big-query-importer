bbf83f71fb7990eb36b5bb5ec0ff32b1c54aa289
Dalia Abuadas <dmabuada@users.noreply.github.com>
2017-07-11 16:36:42 -0400

    Add wordCount to our schema and transform wordCount data

--- modules/page/transform.js
+++ modules/page/transform.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const _ = require('lodash');
+const _ = require('lodash'),
+  stripTags = require('stripTags'),
+  count = require('word-count');
 
 /**
  * Resolve object values, e.g. [{text:tag}{text:tag}] becomes [tag, tag]
@@ -12,6 +14,14 @@ function resolveObj(items) {
 }
 
 /**
+ * Calculate the sum of words in an arr of strings
+ * @param {[]} items
+ * @returns {integer}
+ */
+function countTotalWords(items) {
+  return items.reduce((prev, next) => prev + next, 0);
+}
+/**
  * Mapping function instanceJson -> big query object
  * Business logic goes here
  * @param {string} instanceUri e.g. `some-site.com/pages/an-instance@published.json`
@@ -21,13 +31,17 @@ function resolveObj(items) {
 function articleToBigQuery(instanceUri, instanceJson) {
   // console.log('what is instanceJson', instanceJson)
   let pageData = {},
-    articleFields = ['date', 'canonicalUrl', 'primaryHeadline', 'seoHeadline', 'overrideHeadline', 'shortHeadline', 'syndicatedUrl', 'featureTypes', 'tags', 'contentChannel', 'authors', 'rubric', 'magazineIssueDate'],
+    articleFields = ['date', 'canonicalUrl', 'primaryHeadline', 'seoHeadline', 'overrideHeadline', 'shortHeadline', 'syndicatedUrl', 'featureTypes', 'tags', 'contentChannel', 'authors', 'rubric', 'magazineIssueDate', 'content'],
     headFields = ['twitterTitle', 'ogTitle', 'syndicatedUrl'],
     headLayoutFields = ['siteName', 'pageType', 'vertical'],
     getMainArticleData = _.pick(_.get(instanceJson, 'main[0]', {}), articleFields),
     getSplashHeaderData = _.get(instanceJson, 'splashHeader[0]', {}),
     getHeadLayoutData = _.get(instanceJson, 'headLayout', {}),
     getHeadData = _.get(instanceJson, 'head', {}),
+    resolvedArticleContent,
+    filteredArticleContent,
+    wordsInArticleContent,
+    totalWordsInArticleContent,
     headData,
     headLayoutData,
     filteredHeadData,
@@ -38,21 +52,36 @@ function articleToBigQuery(instanceUri, instanceJson) {
   headLayoutData = _.map(getHeadLayoutData, item => _.pick(item, headLayoutFields));
   filteredHeadLayoutData = headLayoutData.filter(value => Object.keys(value).length !== 0);
 
-  // Assign headData, headLayoutData, splashHeaderData, and mainData to the articlesData obj
+  // Assign headData, headLayoutData, splashHeaderData, and mainData to the pageData obj
   Object.assign(pageData, filteredHeadData[0], filteredHeadLayoutData[0], getSplashHeaderData, getMainArticleData);
 
+  // Resolve article content, strip html and whitespace, and count # of words
+  resolvedArticleContent = _.map(resolveObj(pageData.content), item => stripTags(item));
+  filteredArticleContent = resolvedArticleContent.filter(item => String(item).trim());
+  wordsInArticleContent = _.map(filteredArticleContent, item => count(item));
+  totalWordsInArticleContent = countTotalWords(wordsInArticleContent);
   pageData.pageUri = 'http://' + instanceUri;
   pageData.cmsSource = 'clay';
+
   if (pageData.authors) {
     pageData.authors = resolveObj(pageData.authors);
   }
   if (pageData.tags) {
     pageData.tags = resolveObj(pageData.tags.items);
   }
+  // Only display a word count for pages that have article content
+  if (pageData.content) {
+    // Calculate total # of words in article content and page-level fields
+    // TODO: clean this up
+    pageData.wordCount = _.sum([totalWordsInArticleContent, count(pageData.ogTitle), count(pageData.primaryHeadline), count(pageData.shortHeadline)]);
+  }
+
   pageData.featureTypes = _.keys(_.pickBy(pageData.featureTypes));
   pageData.domain = 'nymag.com';
 
-  console.log('what is our page data', pageData);
+  // Remove content because we don't need to import it to big query
+  pageData = _.omit(pageData, 'content');
+
   return pageData;
 }
 
