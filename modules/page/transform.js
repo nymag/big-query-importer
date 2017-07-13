3bc3e35feb15ce18bf3c39ff3a88aef002414969
Dalia Abuadas <dmabuada@users.noreply.github.com>
2017-07-13 18:01:54 -0400

    Resolve product ids and product buyrls on each page

--- modules/page/transform.js
+++ modules/page/transform.js
@@ -2,7 +2,8 @@
 
 const _ = require('lodash'),
   stripTags = require('stripTags'),
-  count = require('word-count');
+  count = require('word-count'),
+  product = '/components/product';
 
 /**
  * Resolve object values, e.g. [{text:tag}{text:tag}] becomes [tag, tag]
@@ -10,17 +11,25 @@ const _ = require('lodash'),
  * @returns {Array}
  */
 function resolveObj(items) {
-  return items.reduce((arr, item) => arr.concat(item.text), []);
+  return items.reduce((arr, item) => {
+    if (item.description && item.description.length > 0) {
+      // product components have description fields
+      return arr.concat(item.text, item.description[0].text);
+    } else { 
+      return arr.concat(item.text);
+    }
+  }, []);
 }
 
 /**
- * Calculate the sum of words in an arr of strings
- * @param {[]} items
- * @returns {integer}
+ * Resolve a specified object property within article content, e.g. [{ref:uri}{ref:uri}] becomes [uri, uri]
+ * @param {[{}]} items
+ * @returns {Array}
  */
-function countTotalWords(items) {
-  return items.reduce((prev, next) => prev + next, 0);
+function resolveObjProperty(items, property) {
+  return items.reduce((arr, item) => arr.concat(item[property]), []);
 }
+
 /**
  * Mapping function instanceJson -> big query object
  * Business logic goes here
@@ -39,8 +48,10 @@ function articleToBigQuery(instanceUri, instanceJson) {
     getHeadLayoutData = _.get(instanceJson, 'headLayout', {}),
     getHeadData = _.get(instanceJson, 'head', {}),
     resolvedArticleContent,
+    resolvedArticleRefs,
+    resolvedArticleProductRefs,
+    resolvedArticleProductBuyUrls,
     filteredArticleContent,
-    wordsInArticleContent,
     totalWordsInArticleContent,
     headData,
     headLayoutData,
@@ -48,37 +59,46 @@ function articleToBigQuery(instanceUri, instanceJson) {
     filteredHeadLayoutData;
 
   headData = _.map(getHeadData, item => _.pick(item, headFields));
-  filteredHeadData = headData.filter(value => Object.keys(value).length !== 0);
+  filteredHeadData = _.compact(headData);
   headLayoutData = _.map(getHeadLayoutData, item => _.pick(item, headLayoutFields));
-  filteredHeadLayoutData = headLayoutData.filter(value => Object.keys(value).length !== 0);
+  filteredHeadLayoutData = _.compact(headLayoutData);
 
   // Assign headData, headLayoutData, splashHeaderData, and mainData to the pageData obj
   Object.assign(pageData, filteredHeadData[0], filteredHeadLayoutData[0], getSplashHeaderData, getMainArticleData);
 
-  // Resolve article content, strip html and whitespace, and count # of words
+  // Strip html, remove falsey values, and count # of words
   resolvedArticleContent = _.map(resolveObj(pageData.content), item => stripTags(item));
-  filteredArticleContent = resolvedArticleContent.filter(item => String(item).trim());
-  wordsInArticleContent = _.map(filteredArticleContent, item => count(item));
-  totalWordsInArticleContent = countTotalWords(wordsInArticleContent);
-  pageData.pageUri = 'http://' + instanceUri;
-  pageData.cmsSource = 'clay';
+  filteredArticleContent = _.compact(resolvedArticleContent);
+  totalWordsInArticleContent = count(filteredArticleContent.toString());
 
-  if (pageData.authors) {
-    pageData.authors = resolveObj(pageData.authors);
-  }
-  if (pageData.tags) {
-    pageData.tags = resolveObj(pageData.tags.items);
-  }
-  // Only display a word count for pages that have article content
+  // Get all product refs and buy urls on the page
+  resolvedArticleRefs = _.compact(resolveObjProperty(pageData.content, '_ref'));
+  resolvedArticleProductRefs = _.filter(resolvedArticleRefs, function(x) {return x.indexOf(product) !== -1});
+  resolvedArticleProductBuyUrls = _.compact(resolveObjProperty(pageData.content, 'buyUrls'));
+
+    // Only display a word count for pages that have article content
   if (pageData.content) {
     // Calculate total # of words in article content and page-level fields
     // TODO: clean this up
     pageData.wordCount = _.sum([totalWordsInArticleContent, count(pageData.ogTitle), count(pageData.primaryHeadline), count(pageData.shortHeadline)]);
   }
 
+  if (pageData.authors) {
+    pageData.authors = resolveObj(pageData.authors);
+  }
+
+  if (pageData.tags) {
+    pageData.tags = resolveObj(pageData.tags.items);
+  }
+
+  pageData.productIds = resolvedArticleProductRefs;
+  pageData.productBuyUrls = resolvedArticleProductBuyUrls;
+  pageData.pageUri = 'http://' + instanceUri;
+  pageData.cmsSource = 'clay';
   pageData.featureTypes = _.keys(_.pickBy(pageData.featureTypes));
   pageData.domain = 'nymag.com';
 
+  // console.log('what is page data, pageData', pageData);
   // Remove content because we don't need to import it to big query
   pageData = _.omit(pageData, 'content');
 
