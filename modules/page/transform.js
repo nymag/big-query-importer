5f44503665173167da754e72cd8ebf8742bc613d
Dalia Abuadas <dmabuada@users.noreply.github.com>
2017-08-30 17:20:09 -0400

    Update schema

--- modules/page/transform.js
+++ modules/page/transform.js
@@ -14,16 +14,14 @@ const _ = require('lodash'),
  * @returns {Array}
  */
 function resolveObj(items) {
-  if (items !== undefined) {
-    return items.reduce((arr, item) => {
-      if (item.description && item.description.length > 0) {
-        // product components have description fields
-        return arr.concat(item.text, item.description[0].text);
-      } else { 
-        return arr.concat(item.text);
-      }
-    }, []);
-  }
+  return items.reduce((arr, item) => {
+    if (item.description && item.description.length > 0) {
+      // product components have description fields
+      return arr.concat(item.text, item.description[0].text);
+    } else { 
+      return arr.concat(item.text);
+    }
+  }, []);
 }
 
 /**
@@ -32,9 +30,7 @@ function resolveObj(items) {
  * @returns {Array}
  */
 function resolveObjProperty(items, property) {
-  if (items !== undefined) {
-    return items.reduce((arr, item) => arr.concat(item[property]), []);
-  }
+  return items.reduce((arr, item) => arr.concat(item[property]), []);
 }
 
 /**
@@ -46,7 +42,7 @@ function resolveObjProperty(items, property) {
  */
 function articleToBigQuery(instanceUri, instanceJson) {
   let pageData = {},
-    instanceUriHost = 'http://' + instanceUri,
+    instanceUriHost = urls.parse(instanceUri).hostname,
     articleFields = ['date', 'canonicalUrl', 'primaryHeadline', 'seoHeadline', 'overrideHeadline', 'shortHeadline', 'syndicatedUrl', 'featureTypes', 'tags', 'contentChannel', 'authors', 'rubric', 'magazineIssueDate', 'content'],
     headFields = ['twitterTitle', 'ogTitle', 'syndicatedUrl'],
     headLayoutFields = ['siteName', 'pageType', 'vertical'],
@@ -58,30 +54,24 @@ function articleToBigQuery(instanceUri, instanceJson) {
     resolvedArticleRefs,
     resolvedArticleProductRefs,
     resolvedArticleProductBuyUrls,
-    filteredArticleContent,
     totalWordsInArticleContent,
     headData,
-    headLayoutData,
-    filteredHeadData,
-    filteredHeadLayoutData;
+    headLayoutData;
 
-  headData = _.map(getHeadData, item => _.pick(item, headFields));
-  filteredHeadData = _.compact(headData);
-  headLayoutData = _.map(getHeadLayoutData, item => _.pick(item, headLayoutFields));
-  filteredHeadLayoutData = _.compact(headLayoutData);
+  headData = _.compact(_.map(getHeadData, item => _.pick(item, headFields)));
+  headLayoutData = _.compact(_.map(getHeadLayoutData, item => _.pick(item, headLayoutFields)));
 
   // Assign headData, headLayoutData, splashHeaderData, and mainData to the pageData obj
-  Object.assign(pageData, filteredHeadData[0], filteredHeadLayoutData[0], getMainArticleData);
+  Object.assign(pageData, headData[0], headLayoutData[0], getMainArticleData);
 
   // Strip html, remove falsey values, and count # of words
-  filteredArticleContent = _.map(_.compact(resolveObj(pageData.content)), item => stripTags(item));
-  totalWordsInArticleContent = _.map(_.compact([filteredArticleContent.toString(), pageData.ogTitle, pageData.primaryHeadline, pageData.shortHeadline]), item => count(item));
-
+  resolvedArticleContent = _.map(resolveObj(_.compact(pageData.content)), item => stripTags(item));
+  totalWordsInArticleContent = _.map(_.compact([resolvedArticleContent.toString(), pageData.ogTitle, pageData.primaryHeadline, pageData.shortHeadline]), item => count(item));
 
   // Get all product refs and buy urls on the page
-  resolvedArticleRefs = _.compact(resolveObjProperty(pageData.content, '_ref'));
+  resolvedArticleRefs = resolveObjProperty(_.compact(pageData.content), '_ref');
   resolvedArticleProductRefs = _.filter(resolvedArticleRefs, function(x) {return x.indexOf(product) !== -1});
-  resolvedArticleProductBuyUrls = _.compact(resolveObjProperty(pageData.content, 'buyUrls'));
+  resolvedArticleProductBuyUrls = _.compact(resolveObjProperty(_.compact(pageData.content), 'buyUrlHistory'));
 
   // Calculate total # of words in article content and page-level fields
   pageData.wordCount = totalWordsInArticleContent.reduce(function(sum, val) { return sum + val; }, 0)
@@ -97,24 +87,23 @@ function articleToBigQuery(instanceUri, instanceJson) {
   if (pageData.tags) {
     pageData.tags = resolveObj(pageData.tags.items);
   }
-
-  // TODO: ugly, so fix
-  pageData.ogTitle = pageData.ogTitle || '';
-  pageData.pageType = pageData.pageType || '';
-  pageData.siteName = pageData.siteName || '';
-  pageData.twitterTitle = pageData.twitterTitle || '';
-  pageData.vertical = pageData.vertical || '';
-  pageData.contentChannel = pageData.contentChannel || '';
-  pageData.primaryHeadline = stripTags(pageData.primaryHeadline) || '';
+  
+  pageData.ogTitle = stripTags(pageData.ogTitle);
+  pageData.pageType = pageData.pageType;
+  pageData.siteName = pageData.siteName;
+  pageData.twitterTitle = stripTags(pageData.twitterTitle);
+  pageData.vertical = pageData.vertical;
+  pageData.contentChannel = pageData.contentChannel;
+  pageData.primaryHeadline = stripTags(pageData.primaryHeadline);
   pageData.productIds = resolvedArticleProductRefs;
   pageData.productBuyUrls = resolvedArticleProductBuyUrls;
   pageData.pageUri = instanceUri;
   pageData.cmsSource = 'clay';
   pageData.featureTypes = _.keys(_.pickBy(pageData.featureTypes));
-  pageData.domain = urls.parse(instanceUri).hostname;
+  pageData.domain = instanceUriHost;
 
   // Add a timestamp for every entry creation
-  pageData.timestamp = new Date().toISOString();
+  pageData.timestamp = new Date();
 
   // Remove content because we don't need to import it to big query
   pageData = _.omit(pageData, 'content');
